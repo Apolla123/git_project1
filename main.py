7a7702e03f00497197aa7ba05a9da41da96a590c
Apolla123 <apollagames@gmail.com>
2023-11-27 18:31:04 +0300

    готовая программа

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QSpinBox
 import sqlite3
 
 
@@ -12,14 +12,12 @@ class CoffeeApp(QMainWindow):
 
         self.setWindowTitle("Кофе")
 
-        # Создание главного виджета и размещение его на главном окне
         self.centralWidget = QWidget(self)
         self.setCentralWidget(self.centralWidget)
 
-        # Создание вертикального контейнера для размещения виджетов
         layout = QVBoxLayout(self.centralWidget)
 
-        layout.addWidget(self.ui.id_label)
+        layout.addWidget(self.ui.id_spinbox)
         layout.addWidget(self.ui.name_label)
         layout.addWidget(self.ui.roast_label)
         layout.addWidget(self.ui.grind_label)
@@ -27,39 +25,42 @@ class CoffeeApp(QMainWindow):
         layout.addWidget(self.ui.price_label)
         layout.addWidget(self.ui.volume_label)
 
-        # Получение информации о кофе из базы данных
         coffee_info = self.get_coffee_info()
 
-        # Отображение информации о кофе
-        for info in coffee_info:
-            self.ui.id_label.setText(f"ID: {info['ID']}")
-            self.ui.name_label.setText(f"Название сорта: {info['Название сорта']}")
-            self.ui.roast_label.setText(f"Степень обжарки: {info['Степень обжарки']}")
-            self.ui.grind_label.setText(f"Молотый/в зернах: {info['Молотый/в зернах']}")
-            self.ui.taste_label.setText(f"Описание вкуса: {info['Описание вкуса']}")
-            self.ui.price_label.setText(f"Цена: {info['Цена']}")
-            self.ui.volume_label.setText(f"Объем упаковки: {info['Объем упаковки']}")
+        self.ui.id_spinbox.setRange(1, len(coffee_info))
+        self.ui.id_spinbox.valueChanged.connect(self.update_coffee_info)
+
+        self.update_coffee_info()
 
     def get_coffee_info(self):
-        # Подключение к базе данных
         connection = sqlite3.connect('coffee.sqlite')
         cursor = connection.cursor()
 
-        # Выполнение SQL-запроса для извлечения данных
         cursor.execute("SELECT * FROM coffe")
 
-        # Получение результатов запроса
         coffee_info = []
         for row in cursor.fetchall():
             info = {'ID': row[0], 'Название сорта': row[1], 'Степень обжарки': row[2], 'Молотый/в зернах': row[3],
                     'Описание вкуса': row[4], 'Цена': row[5], 'Объем упаковки': row[6]}
             coffee_info.append(info)
 
-        # Закрытие соединения с базой данных
         cursor.close()
         connection.close()
 
         return coffee_info
+
+    def update_coffee_info(self):
+        selected_id = self.ui.id_spinbox.value()
+        coffee_info = self.get_coffee_info()
+
+        for info in coffee_info:
+            if info['ID'] == selected_id:
+                self.ui.name_label.setText(f"Название сорта: {info['Название сорта']}")
+                self.ui.roast_label.setText(f"Степень обжарки: {info['Степень обжарки']}")
+                self.ui.grind_label.setText(f"Молотый/в зернах: {info['Молотый/в зернах']}")
+                self.ui.taste_label.setText(f"Описание вкуса: {info['Описание вкуса']}")
+                self.ui.price_label.setText(f"Цена: {info['Цена']}")
+                self.ui.volume_label.setText(f"Объем упаковки: {info['Объем упаковки']}")
 
 
 if __name__ == "__main__":
